e3f6fcf9efb1220fc66d554e1f93216141c3bccd
srijan2002 <srjnmajumdar8@gmail.com>
2022-06-02 13:12:19 +0530

    Added Cors policy

--- payment_gateway_backend/server.js
+++ payment_gateway_backend/server.js
@@ -10,4 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.listen(port);
 console.log(`http://localhost:${port}`);
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+    });
 app.use(routes);
